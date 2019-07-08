d72ee8c1f1379798a85371846c006fbb107852ee
Cloud User <will@stackhpc.com>
2019-07-08 15:59:47 +0000

    Updated blacklists

---
+++ tempest-tests-in-report.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Usage: ./tempest-tests-in-report --uuid myuuid 
+
+scratch=$(mktemp)
+
+function finish {
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
+rally verify report --type json --to $scratch $@ >/dev/null 2>&1; cat $scratch |  jq -r '.tests | select(.[].by_verification[].status == "fail") | keys | .[]'
